4b3cb3bbec40fda1b853c7ef3c61c0226035de48
sadmanador <sadman.ador@gmail.com>
2024-12-23 12:18:42 +0600

    sidebar and navbar correction done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar = () => {
 
   return (
     <nav className="flex justify-between items-center py-2 px-4 shadow-md bg-white sticky top-0 z-10">
-      <div className="flex items-center mr-1">
+      <div className="flex items-center">
         <Image
           width={22}
           height={22}
           src="/assets/menu.png"
           alt="Menu"
-          className="hidden md:block w-5 mr-6 cursor-pointer"
+          className="w-5 mr-6 cursor-pointer"
           onClick={() => setSidebar(!sidebar)}
         />
         <Link href="/">
@@ -40,20 +40,13 @@ const Navbar = () => {
             height={31}
             src="/assets/logo.png"
             alt="Logo"
-            className="w-32 hidden md:block"
-          />
-          <Image
-            width={32}
-            height={32}
-            src="/assets/logo-small.png"
-            alt="Logo"
-            className=" md:hidden"
+            className="w-32"
           />
         </Link>
       </div>
 
       <div className="flex items-center flex-grow justify-center">
-        <div className="flex items-center border border-gray-300 py-2 px-3 rounded-full mr-4 w-full md:max-w-md">
+        <div className="flex items-center border border-gray-300 py-2 px-3 rounded-full mr-4 w-full max-w-md">
           <input
             type="text"
             placeholder="Search"
@@ -62,14 +55,18 @@ const Navbar = () => {
             onKeyDown={handleKeyDown}
             className="w-full bg-transparent border-none outline-none"
           />
-          <Image
-            width={16}
-            height={16}
-            src="/assets/search.png"
-            alt="Search"
-            className="w-4 cursor-pointer"
+          <div
+            className="hover:bg-gray-200 p-2 rounded-full cursor-pointer active:bg-gray-300"
             onClick={handleSearch}
-          />
+          >
+            <Image
+              width={16}
+              height={16}
+              src="/assets/search.png"
+              alt="Search"
+              className="w-4 "
+            />
+          </div>
         </div>
       </div>
 
@@ -79,28 +76,28 @@ const Navbar = () => {
           height={24}
           src="/assets/upload.png"
           alt="Upload"
-          className="w-4 md:w-6 md:ml-6"
+          className="w-6 ml-6 cursor-pointer"
         />
         <Image
           width={24}
           height={24}
           src="/assets/more.png"
           alt="More"
-          className="w-4 md:w-6 md:ml-6"
+          className="w-6 ml-6 cursor-pointer"
         />
         <Image
           width={24}
           height={24}
           src="/assets/notification.png"
           alt="Notification"
-          className="w-4 md:w-6 md:ml-6"
+          className="w-6 ml-6 cursor-pointer"
         />
         <Image
           width={32}
           height={32}
           src="/assets/jack.png"
           alt="Profile"
-          className="w-6 h-6 md:w-8 md:h-8 rounded-full md:ml-6"
+          className="w-8 h-8 rounded-full ml-6 cursor-pointer"
         />
       </div>
     </nav>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { SidebarToggleContext } from "@/context/SidebarContext/SidebarContext";
 import React, { useContext } from "react";
 import Image from "next/image";
+import { MenuItems, Subscriptions } from "@/utils/sidebar_options";
 
 const Sidebar = () => {
   const { sidebar, category, setCategory } = useContext(SidebarToggleContext);
@@ -8,168 +9,34 @@ const Sidebar = () => {
   return (
     <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
       <div className="shortcut-links">
-        <div
-          className={`side-link ${category === 0 ? "active" : ""}`}
-          onClick={() => setCategory(0)}
-        >
-          <Image width={25} height={25} src="/assets/home.png" alt="Menu" />
-          <p>Home</p>
-        </div>
-        <div
-          className={`side-link ${category === 20 ? "active" : ""}`}
-          onClick={() => setCategory(20)}
-        >
-          <Image
-            width={25}
-            height={25}
-            src="/assets/game_icon.png"
-            alt="Gaming"
-          />
-          <p>Gaming</p>
-        </div>
-        <div
-          className={`side-link ${category === 2 ? "active" : ""}`}
-          onClick={() => setCategory(2)}
-        >
-          <Image
-            width={25}
-            height={25}
-            src="/assets/automobiles.png"
-            alt="Automobiles"
-          />
-          <p>Automobiles</p>
-        </div>
-        <div
-          className={`side-link ${category === 17 ? "active" : ""}`}
-          onClick={() => setCategory(17)}
-        >
-          <Image width={25} height={25} src="/assets/sports.png" alt="Sports" />
-          <p>Sports</p>
-        </div>
-        <div
-          className={`side-link ${category === 24 ? "active" : ""}`}
-          onClick={() => setCategory(24)}
-        >
-          <Image
-            width={25}
-            height={25}
-            src="/assets/entertainment.png"
-            alt="Entertainment"
-          />
-          <p>Entertainment</p>
-        </div>
-        <div
-          className={`side-link ${category === 28 ? "active" : ""}`}
-          onClick={() => setCategory(28)}
-        >
-          <Image
-            width={25}
-            height={25}
-            src="/assets/tech.png"
-            alt="Technology"
-          />
-          <p>Technology</p>
-        </div>
-        <div
-          className={`side-link ${category === 10 ? "active" : ""}`}
-          onClick={() => setCategory(10)}
-        >
-          <Image width={25} height={25} src="/assets/music.png" alt="Music" />
-          <p>Music</p>
-        </div>
-        <div
-          className={`side-link ${category === 22 ? "active" : ""}`}
-          onClick={() => setCategory(22)}
-        >
-          <Image width={25} height={25} src="/assets/blogs.png" alt="Blogs" />
-          <p>Blogs</p>
-        </div>
-        <div
-          className={`side-link ${category === 25 ? "active" : ""}`}
-          onClick={() => setCategory(25)}
-        >
-          <Image width={25} height={25} src="/assets/news.png" alt="News" />
-          <p>News</p>
-        </div>
+        {MenuItems.map((item) => (
+          <div
+            key={item.id}
+            className={`side-link side-menu ${category === item.id ? "active" : ""}`}
+            onClick={() => setCategory(item.id)}
+          >
+            <Image width={25} height={25} src={item.icon} alt={item.label} />
+            <p>{item.label}</p>
+          </div>
+        ))}
         <hr />
       </div>
+
       <div className="subscribed-list">
         <h3>Subscribed</h3>
-        <div className="side-link">
-          <Image
-            width={25}
-            height={25}
-            src="/assets/jack.png"
-            alt="PewDiePie"
-          />
-          <p>PewDiePie</p>
-          <Image
-            width={15}
-            height={15}
-            src="/assets/streaming.png"
-            className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
-            alt="PewDiePie"
-          />
-        </div>
-        <div className="side-link">
-          <Image width={25} height={25} src="/assets/simon.png" alt="MrBeast" />
-          <p>MrBeast</p>
-          <Image
-            width={15}
-            height={15}
-            src="/assets/streaming.png"
-            className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
-            alt="PewDiePie"
-          />
-        </div>
-        <div className="side-link">
-          <Image
-            width={25}
-            height={25}
-            src="/assets/tom.png"
-            alt="Justin Bieber"
-          />
-          <p>Justin Bieber</p>
-          <Image
-            width={15}
-            height={15}
-            src="/assets/streaming.png"
-            className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
-            alt="PewDiePie"
-          />
-        </div>
-        <div className="side-link">
-          <Image
-            width={25}
-            height={25}
-            src="/assets/megan.png"
-            alt="5-Minute Crafts"
-          />
-          <p>Radbrad</p>
-          <Image
-            width={15}
-            height={15}
-            src="/assets/streaming.png"
-            className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
-            alt="PewDiePie"
-          />
-        </div>
-        <div className="side-link">
-          <Image
-            width={25}
-            height={25}
-            src="/assets/cameron.png"
-            alt="Nas Daily"
-          />
-          <p>Nas Daily</p>
-          <Image
-            width={15}
-            height={15}
-            src="/assets/streaming.png"
-            className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
-            alt="PewDiePie"
-          />
-        </div>
+        {Subscriptions.map((sub) => (
+          <div className="side-link" key={sub.name}>
+            <Image width={25} height={25} src={sub.icon} alt={sub.name} />
+            <p>{sub.name}</p>
+            <Image
+              width={15}
+              height={15}
+              src="/assets/streaming.png"
+              className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}
+              alt={sub.name}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
